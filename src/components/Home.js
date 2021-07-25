41b4e3c6e4c15dcc6133ba38a0096a52508b50b7
JoshuaLingbloom <Lingbloomlmp@gmail.com>
2021-07-25 15:38:00 -0700

    commit tree 77dd2f60bb6ddc805097c63f63d5039d6c324fa1

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,10 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+  <div id='home' >
+    <h1 style='color:firebrick'> {name} is a Web Developer from {city}</h1>
+  </div>
+
   // update the JSX being returned!
   return <div>Home</div>;
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,11 @@
 import React from "react";
-
+import Home from './Home'
+import About from './About'
 function NavBar() {
+  <nav>
+    <a href = '#home'> Home </a>
+    <a href='#about'> About </a>
+  </nav>
   // update the JSX being returned!
   return <nav>NavBar</nav>;
 }
